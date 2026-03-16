9c97023ac85e595e33fb864e8a535392766408c3
ThomasNow Productions <thomasnowprod@proton.me>
2026-03-16 20:47:41 +0100

    v2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "eu.ottop.yamlauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "2.1"
+        versionCode = 16
+        versionName = "2.2"
     }
 
     dependenciesInfo {
